623801bfed7166471f4b28c8b05a7fb0da4935d9
Michael Tryby <tryby.michael@epa.gov>
2018-05-17 10:53:52 -0400

    Rewrote test_reeent using Boost threads

--- tests/test_reent.cpp
+++ tests/test_reent.cpp
@@ -1,82 +1,56 @@
 
 #include <string>
-#include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
+
+#include <boost/thread.hpp>
+
 #include "swmm5.h"
 
 #define NUM_THREADS 2
 
 using namespace std;
 
-void *swmm_thread(void *threadid)
+void swmmThread(long i)
 {
-    long tid;
+    int errorcode = 0;
     SWMM_ProjectHandle ph;
 
-    long errorcode = 0;
-    tid = (intptr_t)threadid;
-
-    string prefix = "example1_";
+    string prefix = "example_";
     string suffix = ".inp";
-    string input = prefix + to_string(tid) + suffix;
+    string input = prefix + to_string(static_cast<long long>(i)) + suffix;
 
     suffix = ".rpt";
-    string report = prefix + to_string(tid) + suffix;
+    string report = prefix + to_string(static_cast<long long>(i)) + suffix;
 
     suffix = ".out";
-    string output = prefix + to_string(tid) + suffix;
+    string output = prefix + to_string(static_cast<long long>(i)) + suffix;
 
-    printf("Thread #%ld starting SWMM ...\n", tid);
+    printf("Thread #%ld starting SWMM ...\n", i);
 
     swmm_alloc_project(&ph);
     errorcode = swmm_run_project(ph, input.c_str(), report.c_str(), output.c_str());
     swmm_free_project(&ph);
 
-    printf("Thread #%ld SWMM done. Status = %ld\n", tid, errorcode);
-    pthread_exit((void *)(intptr_t)errorcode);
-
-    return((void *)(intptr_t)errorcode);
+    printf("Thread #%ld SWMM done. Status = %ld\n", i, errorcode);
 }
 
 int main(int argc, char *argv[])
 {
-    pthread_t threads[NUM_THREADS];
-    pthread_attr_t attr;
+    long i;
+    boost::thread *threads[NUM_THREADS];
 
-    int rc;
-    long t;
-    long status = 0;
-
-    /* Initialize and set thread detached attribute */
-    pthread_attr_init(&attr);
-    pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);
-
-    for(t = 0; t < NUM_THREADS; t++){
-        printf("In main: creating thread %ld\n", t);
-        rc = pthread_create(&threads[t], &attr, swmm_thread, (void *)(intptr_t)t);
-        if (rc){
-            printf("ERROR; return code from pthread_create() is %d\n", rc);
-            exit(-1);
-        }
+    for (i = 0; i < NUM_THREADS; i++) {
+        threads[i] = new boost::thread(swmmThread, i);
+        printf("Main: creating thread %ld.\n", i);
     }
 
-    /* Free attribute and wait for the other threads */
-    pthread_attr_destroy(&attr);
-
-    for(t = 0; t < NUM_THREADS; t++) {
-        rc = pthread_join(threads[t], NULL); //(void **)(intptr_t)status);
-        if (rc) {
-            printf("ERROR; return code from pthread_join() is %d\n", rc);
-            exit(-1);
-        }
-
-        printf("Main: completed join with thread %ld having a status of %ld\n",
-                t, status);
+    for (i = 0; i < NUM_THREADS; i++) {
+        threads[i]->join();
+        printf("Main: joining thread %ld.\n", i);
+        delete threads[i];
     }
 
     printf("Main: program completed. Exiting.\n");
-    pthread_exit(NULL);
     return(0);
 }
